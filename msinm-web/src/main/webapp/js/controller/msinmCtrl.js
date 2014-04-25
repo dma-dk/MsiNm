54d7964414368d45adb23f847b9be1481793fb22
peder280370 <peder@carolus.dk>
2014-04-25 13:48:07 +0200

    Improved filtering

--- msinm-web/src/main/webapp/js/controller/msinmCtrl.js
+++ msinm-web/src/main/webapp/js/controller/msinmCtrl.js
@@ -7,16 +7,31 @@ angular.module('msinm')
         function ($scope, $routeParams, MsiNmService) {
         'use strict';
 
+        $scope.query = undefined;
         $scope.msinmList = [];
+        $scope.filteredList = [];
         $scope.predicate = '-issueDate';
 
         $scope.loadMsiNm = function () {
             MsiNmService.list(function(data) {
-                $scope.msinmList = data;
+                $scope.filteredList = $scope.msinmList = data;
+                $scope.selection = '';
             },
             function () {
                 alert("Error");
             });
         };
 
+
+        $scope.onEdit = function () {
+            $scope.filteredList = [];
+            $.each($scope.msinmList, function(index, msi) {
+               var q = $scope.query.toLowerCase();
+               if (msi.messageItems[0].keySubject.toLowerCase().indexOf(q) != -1 ||
+                   msi.generalArea.toLowerCase().indexOf(q) != -1 ||
+                   msi.locality.toLowerCase().indexOf(q) != -1) {
+                   $scope.filteredList.push(msi);
+               }
+            });
+        };
     }]);
