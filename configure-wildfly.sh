569d3fc2f1d87896a352005762478ed99f9595d9
Ole Borup <obo@dma.dk>
2014-07-16 13:46:45 +0200

    Wildfly listening to public interfaces when started from init script

--- configure-wildfly.sh
+++ configure-wildfly.sh
@@ -6,5 +6,6 @@ cp conf/standalone.xml $WILDFLY/standalone/configuration/
 mkdir -p $WILDFLY/modules/com/mysql/main
 cp conf/module.xml $WILDFLY/modules/com/mysql/main/
 cp conf/mysql-connector-java-5.1.30-bin.jar $WILDFLY/modules/com/mysql/main/
+chmod +x $WILDFLY/bin/*.sh
 
 
