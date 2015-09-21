340f2ff061bb21138382a3725d5b7dd45c38aa59
peder280370 <peder@carolus.dk>
2015-09-21 10:35:07 +0200

    Documentation update

--- msinm-legacy-dk/src/main/java/dk/dma/msinm/legacy/aton/LegacyAtonImportRestService.java
+++ msinm-legacy-dk/src/main/java/dk/dma/msinm/legacy/aton/LegacyAtonImportRestService.java
@@ -15,13 +15,8 @@
  */
 package dk.dma.msinm.legacy.aton;
 
-import dk.dma.msinm.common.MsiNmApp;
 import dk.dma.msinm.common.repo.RepositoryService;
-import dk.dma.msinm.legacy.nm.ActiveTempPrelimNmPdfExtractor;
-import dk.dma.msinm.legacy.nm.NmPdfExtractor;
 import dk.dma.msinm.model.AtoN;
-import dk.dma.msinm.model.Message;
-import dk.dma.msinm.model.SeriesIdentifier;
 import dk.dma.msinm.service.AtoNService;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
@@ -30,8 +25,6 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
 import org.slf4j.Logger;
 
 import javax.ejb.Stateless;
@@ -137,6 +130,20 @@ public class LegacyAtonImportRestService {
             aton.setOwner(row.getCell(colIndex.get("EJER")).getStringCellValue());
             aton.setLat(row.getCell(colIndex.get("LATTITUDE")).getNumericCellValue());
             aton.setLon(row.getCell(colIndex.get("LONGITUDE")).getNumericCellValue());
+
+
+            // In this simplified "type" mapping, we map the "karaknr" field to the value 1-3.
+            // "karaknr" values:
+            // 0: AIS
+            // 1: Fyr
+            // 2: Bifyr, tågelys, advarselslys, retningsfyr, hindringslys, m.v.
+            // 3: Båker, Signalmaster
+            // 4: RACONS
+            // 5: Lystønder
+            // 6: Vagere
+            // 7: Stager i bund
+            // 8: Radiofyr
+            // 9: Tågesignaler
 
             String karaknr = String.valueOf(Math.round(row.getCell(colIndex.get("KARAKNR")).getNumericCellValue()));
             if (karaknr.contains("5")) {
